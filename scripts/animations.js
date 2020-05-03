472d010036dd4915cf11a5fe4eb63fccac64062b
zvikarp <zvi@zektec.com>
2020-05-03 19:13:52 +0300

    🚧 added button animation

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $("#topbar").css("opacity", 1 - $(window).scrollTop() / $('#header').height()/0.8);
-    });
+$(document).ready(function () {
+	// fade topbar on scroll
+	$(window).scroll(function () {
+		$("#topbar").css(
+			"opacity",
+			1 - $(window).scrollTop() / $("#header").height() / 0.8
+		);
+	});
+
+	// scroll to section on click mazes button
+	$("#mazes-button").click(function () {
+		$("html, body").animate(
+			{
+				scrollTop: parseInt($("#mazes-section").offset().top),
+			},
+			700
+		);
+	});
 });
