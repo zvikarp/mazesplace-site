f52897c9d3080149080be386a4e31fb12eddc2f2
Restyled.io <commits@restyled.io>
2020-09-11 02:44:09 +0000

    Restyled by clang-format

--- scripts/mazeCreator.js
+++ scripts/mazeCreator.js
@@ -7,53 +7,48 @@
 // }
 
 function canvasToArray(canvas) {
-	const array = Array(100)
-		.fill()
-		.map(() => Array(100));
+  const array = Array(100).fill().map(() => Array(100));
 
-	scale = canvas.width / 100;
-	for (let x = 0; x < 100; x++) {
-		for (let y = 0; y < 100; y++) {
-			const pixelData = canvas.getContext('2d').getImageData(y * scale, x * scale, 1, 1).data;
-			array[x][y] = pixelData[3] == 255;
-		}
-	}
-	return array;
+  scale = canvas.width / 100;
+  for (let x = 0; x < 100; x++) {
+    for (let y = 0; y < 100; y++) {
+      const pixelData =
+          canvas.getContext('2d').getImageData(y * scale, x * scale, 1, 1).data;
+      array[x][y] = pixelData[3] == 255;
+    }
+  }
+  return array;
 }
 
 function createMaze() {
-	const canvas = document.querySelector("#maze-creator-canvas");
-	const createButton = document.getElementById("create-maze");
-	const creatingText = document.getElementById("creating-maze");
-	createButton.style.display = "none";
-	creatingText.style.display = "inline";
-	const array = canvasToArray(canvas);
+  const canvas = document.querySelector("#maze-creator-canvas");
+  const createButton = document.getElementById("create-maze");
+  const creatingText = document.getElementById("creating-maze");
+  createButton.style.display = "none";
+  creatingText.style.display = "inline";
+  const array = canvasToArray(canvas);
 
-	const url = "http://127.0.0.1:5000/create";
-	const params = {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ array }),
-		method: "POST",
-	};
+  const url = "http://127.0.0.1:5000/create";
+  const params = {
+    headers : {
+      Accept : "application/json",
+      "Content-Type" : "application/json",
+    },
+    body : JSON.stringify({array}),
+    method : "POST",
+  };
 
-	fetch(url, params)
-		.then(function (response) {
-			return response.blob();
-		})
-		.then(function (avatarAsBlob) {
-			const objectURL = URL.createObjectURL(avatarAsBlob);
-			creatingText.download = "custom-maze.JPEG";
-			creatingText.href = objectURL;
-			creatingText.click();
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-		.finally((e) => {
-			createButton.style.display = "inline";
-			creatingText.style.display = "none";
-		});
+  fetch(url, params)
+      .then(function(response) { return response.blob(); })
+      .then(function(avatarAsBlob) {
+        const objectURL = URL.createObjectURL(avatarAsBlob);
+        creatingText.download = "custom-maze.JPEG";
+        creatingText.href = objectURL;
+        creatingText.click();
+      })
+      .catch((err) => { console.log(err); })
+      .finally((e) => {
+        createButton.style.display = "inline";
+        creatingText.style.display = "none";
+      });
 }
--- scripts/mazeGenerator.js
+++ scripts/mazeGenerator.js
@@ -1,87 +1,75 @@
 function generateWordMaze() {
-	const generateButton = document.getElementById("generate-maze");
-	const generatingText = document.getElementById("generating-maze");
-	const mazeText = document
-		.getElementById("maze-generator-input")
-		.value.toString();
-	// const url = "https://mazesplace-server.herokuapp.com/";
-	const url = "http://127.0.0.1:5000/generate";
-	const data = { text: mazeText };
-	const params = {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-		method: "POST",
-	};
+  const generateButton = document.getElementById("generate-maze");
+  const generatingText = document.getElementById("generating-maze");
+  const mazeText =
+      document.getElementById("maze-generator-input").value.toString();
+  // const url = "https://mazesplace-server.herokuapp.com/";
+  const url = "http://127.0.0.1:5000/generate";
+  const data = {text : mazeText};
+  const params = {
+    headers : {
+      Accept : "application/json",
+      "Content-Type" : "application/json",
+    },
+    body : JSON.stringify(data),
+    method : "POST",
+  };
 
-	generateButton.style.display = "none";
-	generatingText.style.display = "inline";
+  generateButton.style.display = "none";
+  generatingText.style.display = "inline";
 
-	fetch(url, params)
-		.then(function (response) {
-			return response.blob();
-		})
-		.then(function (avatarAsBlob) {
-			const objectURL = URL.createObjectURL(avatarAsBlob);
-			generatingText.download = "generated-maze.JPEG";
-			generatingText.href = objectURL;
-			generatingText.click();
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-		.finally((e) => {
-			generateButton.style.display = "inline";
-			generatingText.style.display = "none";
-		});
+  fetch(url, params)
+      .then(function(response) { return response.blob(); })
+      .then(function(avatarAsBlob) {
+        const objectURL = URL.createObjectURL(avatarAsBlob);
+        generatingText.download = "generated-maze.JPEG";
+        generatingText.href = objectURL;
+        generatingText.click();
+      })
+      .catch((err) => { console.log(err); })
+      .finally((e) => {
+        generateButton.style.display = "inline";
+        generatingText.style.display = "none";
+      });
 }
 
 function createMaze() {
-	var canvas = document.querySelector("#mainCanvas");
-	const generateButton = document.getElementById("generate-maze");
-	const generatingText = document.getElementById("generating-maze");
-	let array = Array(100)
-		.fill()
-		.map(() => Array(100));
+  var canvas = document.querySelector("#mainCanvas");
+  const generateButton = document.getElementById("generate-maze");
+  const generatingText = document.getElementById("generating-maze");
+  let array = Array(100).fill().map(() => Array(100));
 
-	for (var x = 0; x < 100; x++) {
-		for (var y = 0; y < 100; y++) {
-			var pixelData = canvas
-				.getContext("2d")
-				.getImageData(y * 5, x * 5, 1, 1).data;
-			array[x][y] = pixelData[3] == 255;
-		}
-	}
+  for (var x = 0; x < 100; x++) {
+    for (var y = 0; y < 100; y++) {
+      var pixelData =
+          canvas.getContext("2d").getImageData(y * 5, x * 5, 1, 1).data;
+      array[x][y] = pixelData[3] == 255;
+    }
+  }
 
-	const url = "http://127.0.0.1:5000/create";
-	const data = { array };
-	const params = {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-		method: "POST",
-	};
+  const url = "http://127.0.0.1:5000/create";
+  const data = {array};
+  const params = {
+    headers : {
+      Accept : "application/json",
+      "Content-Type" : "application/json",
+    },
+    body : JSON.stringify(data),
+    method : "POST",
+  };
 
-	fetch(url, params)
-		.then(function (response) {
-			return response.blob();
-		})
-		.then(function (avatarAsBlob) {
-			const objectURL = URL.createObjectURL(avatarAsBlob);
-			generatingText.download = "generated-maze.JPEG";
-			generatingText.href = objectURL;
-			generatingText.click();
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-		.finally((e) => {
-			generateButton.style.display = "inline";
-			generatingText.style.display = "none";
-		});
-	console.log(array);
+  fetch(url, params)
+      .then(function(response) { return response.blob(); })
+      .then(function(avatarAsBlob) {
+        const objectURL = URL.createObjectURL(avatarAsBlob);
+        generatingText.download = "generated-maze.JPEG";
+        generatingText.href = objectURL;
+        generatingText.click();
+      })
+      .catch((err) => { console.log(err); })
+      .finally((e) => {
+        generateButton.style.display = "inline";
+        generatingText.style.display = "none";
+      });
+  console.log(array);
 }
