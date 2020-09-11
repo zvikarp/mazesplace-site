2dadf675523b5c1b56b8968bee4fc7a3726e0897
zvikarp <zvi@zektec.com>
2020-09-11 05:49:33 +0300

    🐛 added the url

--- scripts/mazeGenerator.js
+++ scripts/mazeGenerator.js
@@ -4,7 +4,7 @@ function generateWordMaze() {
 	const mazeText = document
 		.getElementById("maze-generator-input")
 		.value.toString();
-	const url = "https://mazesplace-server.herokuapp.com/";
+	const url = "https://mazesplace-server.herokuapp.com/generate";
 	// const url = "http://127.0.0.1:5000/generate";
 	const data = { text: mazeText };
 	const params = {
