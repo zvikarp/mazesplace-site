3bbe4137ba634620fc2e5e92070868d457a4a167
Restyled.io <commits@restyled.io>
2020-10-05 08:16:22 +0000

    Restyled by clang-format

--- scripts/mazeGenerator.js
+++ scripts/mazeGenerator.js
@@ -1,40 +1,35 @@
 function generateWordMaze() {
-	const generateButton = document.getElementById("generate-maze");
-	const generatingText = document.getElementById("generating-maze");
-	const mazeText = document
-		.getElementById("maze-generator-input")
-		.value.toString();
-	// const url = "https://mazesplace-server.herokuapp.com/generate";
-	const url = "http://127.0.0.1:5000/generate";
-	const data = { text: mazeText };
-	const params = {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-		method: "POST",
-	};
+  const generateButton = document.getElementById("generate-maze");
+  const generatingText = document.getElementById("generating-maze");
+  const mazeText =
+      document.getElementById("maze-generator-input").value.toString();
+  // const url = "https://mazesplace-server.herokuapp.com/generate";
+  const url = "http://127.0.0.1:5000/generate";
+  const data = {text : mazeText};
+  const params = {
+    headers : {
+      Accept : "application/json",
+      "Content-Type" : "application/json",
+    },
+    body : JSON.stringify(data),
+    method : "POST",
+  };
 
-	generateButton.style.display = "none";
-	generatingText.style.display = "inline";
+  generateButton.style.display = "none";
+  generatingText.style.display = "inline";
 
-	fetch(url, params)
-		.then(function (response) {
-			return response.blob();
-		})
-		.then(function (avatarAsBlob) {
-			const objectURL = URL.createObjectURL(avatarAsBlob);
-			generatingText.download = "generated-maze.JPEG";
-			generatingText.href = objectURL;
-			generatingText.click();
-			logEvent("create_maze", "maze_generator", "maze_generator");
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-		.finally((e) => {
-			generateButton.style.display = "inline";
-			generatingText.style.display = "none";
-		});
+  fetch(url, params)
+      .then(function(response) { return response.blob(); })
+      .then(function(avatarAsBlob) {
+        const objectURL = URL.createObjectURL(avatarAsBlob);
+        generatingText.download = "generated-maze.JPEG";
+        generatingText.href = objectURL;
+        generatingText.click();
+        logEvent("create_maze", "maze_generator", "maze_generator");
+      })
+      .catch((err) => { console.log(err); })
+      .finally((e) => {
+        generateButton.style.display = "inline";
+        generatingText.style.display = "none";
+      });
 }
