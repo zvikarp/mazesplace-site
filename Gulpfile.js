06c40fb86d000afc998dea8f0ff705e66e42025f
Restyled.io <commits@restyled.io>
2020-10-23 10:44:37 +0000

    Restyled by clang-format

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,144 +11,129 @@ const config = require("./config");
 
 // fetch command line arguments
 const arg = ((argList) => {
-	let arg = {},
-		a,
-		opt,
-		thisOpt,
-		curOpt;
-	for (a = 0; a < argList.length; a++) {
-		thisOpt = argList[a].trim();
-		opt = thisOpt.replace(/^\-+/, "");
-		if (opt === thisOpt) {
-			if (curOpt) arg[curOpt] = opt;
-			curOpt = null;
-		} else {
-			curOpt = opt;
-			arg[curOpt] = true;
-		}
-	}
-	return arg;
+  let arg = {}, a, opt, thisOpt, curOpt;
+  for (a = 0; a < argList.length; a++) {
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, "");
+    if (opt === thisOpt) {
+      if (curOpt)
+        arg[curOpt] = opt;
+      curOpt = null;
+    } else {
+      curOpt = opt;
+      arg[curOpt] = true;
+    }
+  }
+  return arg;
 })(process.argv);
 
 const env = arg.env || "dev";
 
 // paths
 const paths = {
-	css: {
-		src: "src/sass/**/*.scss",
-		dest: "dist/css/",
-	},
-	html: {
-		src: "src/**/*.html",
-		dest: "dist/",
-	},
-	js: {
-		src: "src/scripts/**/*.js",
-		dest: "dist/scripts/",
-	},
-	assets: {
-		src: "src/assets/**/*",
-		dest: "dist/assets/",
-	},
-	config: {
-		file: "config.js",
-		dest: "dist/",
-	},
+  css : {
+    src : "src/sass/**/*.scss",
+    dest : "dist/css/",
+  },
+  html : {
+    src : "src/**/*.html",
+    dest : "dist/",
+  },
+  js : {
+    src : "src/scripts/**/*.js",
+    dest : "dist/scripts/",
+  },
+  assets : {
+    src : "src/assets/**/*",
+    dest : "dist/assets/",
+  },
+  config : {
+    file : "config.js",
+    dest : "dist/",
+  },
 };
 
 // Gulp task to get config file
-gulp.task("config", function () {
-	const constantsOpts = config[env];
-	let constantsObjString = "";
-	for (key in constantsOpts) {
-		let value = constantsOpts[key];
-		if (typeof value === "string") {
-			value = `'${value}'`;
-		}
-		constantsObjString += `\n${key}: ${value.toString()},`;
-	}
-	const codeString = `const config = { ${constantsObjString} \n}`;
-	return file(paths.config.file, codeString, { src: true }).pipe(
-		gulp.dest(paths.config.dest)
-	);
+gulp.task("config", function() {
+  const constantsOpts = config[env];
+  let constantsObjString = "";
+  for (key in constantsOpts) {
+    let value = constantsOpts[key];
+    if (typeof value === "string") {
+      value = `'${value}'`;
+    }
+    constantsObjString += `\n${key}: ${value.toString()},`;
+  }
+  const codeString = `const config = { ${constantsObjString} \n}`;
+  return file(paths.config.file, codeString, {src : true})
+      .pipe(gulp.dest(paths.config.dest));
 });
 
 // Copies the assets to the distribution.
 gulp.task("assets", function buildImages() {
-	return gulp.src(paths.assets.src).pipe(gulp.dest(paths.assets.dest));
+  return gulp.src(paths.assets.src).pipe(gulp.dest(paths.assets.dest));
 });
 
 // Gulp task to minify CSS files
-gulp.task("styles", function () {
-	return gulp
-		.src(paths.css.src)
-		.pipe(
-			sass({
-				outputStyle: "nested",
-				precision: 10,
-				includePaths: ["."],
-				onError: console.error.bind(console, "Sass error:"),
-			})
-		)
-		.pipe(autoprefixer())
-		.pipe(csso())
-		.pipe(gulp.dest(paths.css.dest));
+gulp.task("styles", function() {
+  return gulp.src(paths.css.src)
+      .pipe(sass({
+        outputStyle : "nested",
+        precision : 10,
+        includePaths : [ "." ],
+        onError : console.error.bind(console, "Sass error:"),
+      }))
+      .pipe(autoprefixer())
+      .pipe(csso())
+      .pipe(gulp.dest(paths.css.dest));
 });
 
 // Gulp task to minify JavaScript files
-gulp.task("scripts", function () {
-	return gulp.src(paths.js.src).pipe(terser()).pipe(gulp.dest(paths.js.dest));
+gulp.task("scripts", function() {
+  return gulp.src(paths.js.src).pipe(terser()).pipe(gulp.dest(paths.js.dest));
 });
 
 // Gulp task to minify HTML files
-gulp.task("html", function () {
-	return gulp
-		.src(paths.html.src)
-		.pipe(
-			htmlmin({
-				collapseWhitespace: true,
-				removeComments: true,
-			})
-		)
-		.pipe(gulp.dest(paths.html.dest));
+gulp.task("html", function() {
+  return gulp.src(paths.html.src)
+      .pipe(htmlmin({
+        collapseWhitespace : true,
+        removeComments : true,
+      }))
+      .pipe(gulp.dest(paths.html.dest));
 });
 
 // Sets up the live browser sync.
 gulp.task("browser-sync", function sync(done) {
-	bs.init({
-		server: {
-			baseDir: "dist/",
-		},
-	});
-	done();
+  bs.init({
+    server : {
+      baseDir : "dist/",
+    },
+  });
+  done();
 });
 
 gulp.task("watch", function watch(done) {
-	gulp.watch(paths.assets.src, gulp.series("rebuild"));
-	gulp.watch(paths.js.src, gulp.series("rebuild"));
-	gulp.watch(paths.html.src, gulp.series("rebuild"));
-	gulp.watch(paths.css.src, gulp.series("rebuild"));
-	gulp.watch(paths.config.file, gulp.series("rebuild"));
-	done();
+  gulp.watch(paths.assets.src, gulp.series("rebuild"));
+  gulp.watch(paths.js.src, gulp.series("rebuild"));
+  gulp.watch(paths.html.src, gulp.series("rebuild"));
+  gulp.watch(paths.css.src, gulp.series("rebuild"));
+  gulp.watch(paths.config.file, gulp.series("rebuild"));
+  done();
 });
 
 // Clean output directory
-gulp.task("clean", () => del(["dist"]));
+gulp.task("clean", () => del([ "dist" ]));
 
 // Gulp task to minify all files
-gulp.task(
-	"build",
-	gulp.series("styles", "scripts", "html", "assets", "config")
-);
+gulp.task("build",
+          gulp.series("styles", "scripts", "html", "assets", "config"));
 
 // First rebuilds the output then triggers a reload of the browser.
-gulp.task(
-	"rebuild",
-	gulp.series("build", function rebuild(done) {
-		bs.reload();
-		done();
-	})
-);
+gulp.task("rebuild", gulp.series("build", function rebuild(done) {
+  bs.reload();
+  done();
+}));
 
 // Gulp task to minify all files
 gulp.task("default", gulp.series("clean", "build", "browser-sync", "watch"));
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 function setVh() {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty("--vh", `${vh}px`);
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
 window.addEventListener("resize", () => setVh);
@@ -8,17 +8,15 @@ window.addEventListener("resize", () => setVh);
 setVh();
 
 function downloadMaze(mazeName) {
-	logEvent("maze", mazeName, mazeName);
-	window.open(
-		`assets/mazes/downloadables/${mazeName}--Downloadable.pdf`,
-		"_blank"
-	);
+  logEvent("maze", mazeName, mazeName);
+  window.open(`assets/mazes/downloadables/${mazeName}--Downloadable.pdf`,
+              "_blank");
 }
 
 function logEvent(type, id, name) {
-	firebase.analytics().logEvent("select_content", {
-		content_type: type,
-		content_id: id,
-		items: [{ name: name }],
-	});
+  firebase.analytics().logEvent("select_content", {
+    content_type : type,
+    content_id : id,
+    items : [ {name : name} ],
+  });
 }
